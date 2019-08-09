ffa3316a67ac696ca8cfeb2f30d30c997edb502f
kvnlay <kingstevens007@gmail.com>
2019-08-09 13:00:33 +0100

    Styled buttons with color and width props

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import '../App.css';
 
 function ButtonPanel() {
   return (
@@ -8,30 +9,30 @@ function ButtonPanel() {
         <Button name="AC" />
         <Button name="+/-" />
         <Button name="%" />
-        <Button name="+" />
+        <Button name="+" color="orange" />
       </div>
       <div className="row">
         <Button name="7" />
         <Button name="8" />
         <Button name="9" />
-        <Button name="X" />
+        <Button name="X" color="orange" />
       </div>
       <div className="row">
         <Button name="4" />
         <Button name="5" />
         <Button name="6" />
-        <Button name="-" />
+        <Button name="-" color="orange" />
       </div>
       <div className="row">
         <Button name="1" />
         <Button name="2" />
         <Button name="3" />
-        <Button name="+" />
+        <Button name="+" color="orange" />
       </div>
       <div className="row">
-        <Button name="0" />
+        <Button name="0" width />
         <Button name="." />
-        <Button name="=" />
+        <Button name="=" color="orange" />
       </div>
     </div>
   );
