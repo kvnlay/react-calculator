87f60f6f7bc57d1ca3a91aecb14982ae2595be83
kvnlay <kingstevens007@gmail.com>
2019-08-12 21:05:19 +0100

    Add calculate module

---
+++ src/logic/calculate.js
@@ -0,0 +1,82 @@
+import operate from './operate';
+
+const calculate = (data, buttonName) => {
+  let newData = Object.assign({}, data);
+  switch (buttonName) {
+    case 'AC':
+      newData = {
+        total: '',
+        next: null,
+        operation: null,
+      };
+      break;
+
+    case '+/-':
+      if (newData.next) {
+        newData.next = (+newData.next * -1).toString();
+      }
+      newData.total = (+newData.total * -1).toString();
+      break;
+
+    case '+':
+    case '-':
+    case 'X':
+    case '÷':
+      if (newData.total && newData.next && newData.operation) {
+        newData.total = operate(newData.total, newData.next, newData.operation);
+        newData.next = null;
+        newData.operation = buttonName;
+      } else if (newData.total && newData.next === null) {
+        newData.operation = buttonName;
+      }
+      break;
+    case '%':
+      if (newData.next) {
+        newData.next = (+newData.next / 100).toString();
+      } else {
+        newData.total = (+newData.total / 100).toString();
+      }
+      break;
+    case '=':
+      if (newData.next) {
+        newData.total = operate(newData.total, newData.next, newData.operation);
+        newData.next = null;
+      }
+      newData.operation = null;
+      break;
+    case '.':
+      if (newData.operation === '=') {
+        newData.total = '0.';
+        newData.next = null;
+        newData.operation = null;
+      }
+      if (newData.next === null && !newData.operation) {
+        if (!newData.total.split('').includes('.')) {
+          newData.total += '.';
+        } else {
+          newData.total = '0.';
+        }
+      } else if (!newData.next.split('').includes('.')) {
+        newData.next += '.';
+      } else {
+        newData.next = '0.';
+      }
+      break;
+
+    default:
+      if (newData.operation === '=') {
+        newData.total = '';
+        newData.operation = null;
+      }
+
+      if (newData.operation === null) {
+        newData.total = newData.total === '0' ? buttonName : (newData.total += buttonName);
+      } else {
+        newData.next += buttonName;
+      }
+      break;
+  }
+  return newData;
+};
+
+export default calculate;
