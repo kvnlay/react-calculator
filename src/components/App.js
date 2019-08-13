4b0a0a1a0c61df228a8f043da368bd235a39c137
kvnlay <kingstevens007@gmail.com>
2019-08-13 15:33:09 +0100

    refactor state and add handleclick method

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      result: '',
-    };
-  }
+  state = {
+    total: '',
+    next: null,
+    operation: null,
+  };
+
+  handleClick = buttonName => {
+    this.setState(prevState => calculate(prevState, buttonName));
+  };
 
   render() {
-    const { result } = this.state;
+    const { total, next } = this.state;
     return (
       <div className="container">
-        <Display result={result} />
-        <ButtonPanel />
+        <Display result={total} next={next} />
+        <ButtonPanel clickHandler={this.handleClick}/>
       </div>
     );
   }
