4bced88666ab3bbeab487d19428b69f59a9eeb2d
kvnlay <kingstevens007@gmail.com>
2019-08-13 15:32:25 +0100

    Pass click event listener to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,21 @@ import '../App.css';
 import PropTypes from 'prop-types';
 
 function Button(props) {
-  const { color = 'gray', width = false, name } = props;
+  const {
+    color = 'gray',
+    width = false,
+    name,
+    clickHandler,
+  } = props;
+
+  const handleClick = () => clickHandler(name);
   return (
-    <div className={`button button-${color} ${width ? 'wide' : ''}`}>
-      <p>{ name }</p>
-    </div>
+    <button
+      className={`button button-${color} ${width ? 'wide' : ''}`}
+      onClick={handleClick}
+    >
+      { name }
+    </button>
   );
 }
 
@@ -15,5 +25,6 @@ Button.propTypes = {
   color: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   width: PropTypes.bool.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 export default Button;
